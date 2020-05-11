385f99fc48b8a4f9e441eb3daf98f9e3ad0c0025
josephmotha1998 <josephmotha@outlook.com>
2020-05-10 21:09:00 -0400

    updated webcam pose detection for more accuracy

--- sketch.js
+++ sketch.js
@@ -1,58 +1,104 @@
-
 let video;
 let poseNet;
-let pose;
-let skeleton;
+let poses = [];
 
 function setup() {
- createCanvas(640,480)
- video=createCapture(VIDEO);
- video.hide(); 
- poseNet=ml5.poseNet(video,modelLoaded);
- poseNet.on('pose',gotPoses);
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.size(width, height);
+
+  // Create a new poseNet method with a single detection
+  poseNet = ml5.poseNet(video, modelReady);
+  // This sets up an event that fills the global variable "poses"
+  // with an array every time new poses are detected
+  poseNet.on('pose', function(results) {
+    poses = results;
+  });
+  // Hide the video element, and just show the canvas
+  video.hide();
 }
 
-function gotPoses(poses){
-    console.log(poses);
-    if (poses.length>0){
-        pose=poses[0].pose;
-        skeleton=poses[0].skeleton;
-    }
-}
-
-function modelLoaded(){
-    console.log("Model is Ready!")
+function modelReady() {
+  select('#status').html('Model Loaded');
 }
 
 function draw() {
-    image(video,0,0);
+  image(video, 0, 0, width, height);
 
-    if(pose){
-        // fill(255,0,0);
-        // ellipse(pose.nose.x,pose.nose.y,64);
-        fill(0,0,255);
-        ellipse(pose.rightWrist.x,pose.rightWrist.y,25);
-        ellipse(pose.leftWrist.x,pose.leftWrist.y,25);
-
-
-        for(let i=0;i<pose.keypoints.length;i++){
-            let x= pose.keypoints[i].position.x;
-            let y= pose.keypoints[i].position.y;
-            fill(0,255,0);
-            ellipse(x,y,16,16);
-    
-        }
-        for (let i=0;i<skeleton.length;i++){
-            let a=skeleton[i][0];
-            let b=skeleton[i][1];
-            strokeWeight(2);
-            stroke(255); 
-            line(a.position.x,a.position.y,b.position.x,b.position.y);
-        }
-    }
-
-    
-
-   
-
+  // We can call both functions to draw all keypoints and the skeletons
+  drawKeypoints();
+  drawSkeleton();
 }
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints()  {
+  // Loop through all the poses detected
+  for (let i = 0; i < poses.length; i++) {
+    // For each pose detected, loop through all the keypoints
+    let pose = poses[i].pose;
+    for (let j = 0; j < pose.keypoints.length; j++) {
+      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+      let keypoint = pose.keypoints[j];
+      // Only draw an ellipse is the pose probability is bigger than 0.2
+      if (keypoint.score > 0.2) {
+        fill(255, 0, 0);
+        noStroke();
+        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+      }
+    }
+  }
+}
+
+// A function to draw the skeletons
+function drawSkeleton() {
+  // Loop through all the skeletons detected
+  for (let i = 0; i < poses.length; i++) {
+    let skeleton = poses[i].skeleton;
+    // For every skeleton, loop through all body connections
+    for (let j = 0; j < skeleton.length; j++) {
+      let partA = skeleton[j][0];
+      let partB = skeleton[j][1];
+      stroke(255, 0, 0);
+      line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+    }
+  }
+}
+// function draw() {
+//     background(240);
+//     let v0 = createVector(50, 50);
+  
+//     let v1 = createVector(50, 0);
+//     drawArrow(v0, v1, 'red');
+  
+//     let v2 = createVector(mouseX - 50, mouseY - 50);
+//     drawArrow(v0, v2, 'blue');
+  
+//     let angleBetween = v1.angleBetween(v2);
+//     noStroke();
+//     text(
+//       'angle between: ' +
+//         angleBetween.toFixed(2) +
+//         ' radians or ' +
+//         degrees(angleBetween).toFixed(2) +
+//         ' degrees',
+//       10,
+//       50,
+//       90,
+//       50
+//     );
+//   }
+  
+//   // draw an arrow for a vector at a given base position
+//   function drawArrow(base, vec, myColor) {
+//     push();
+//     stroke(myColor);
+//     strokeWeight(3);
+//     fill(myColor);
+//     translate(base.x, base.y);
+//     line(0, 0, vec.x, vec.y);
+//     rotate(vec.heading());
+//     let arrowSize = 7;
+//     translate(vec.mag() - arrowSize, 0);
+//     triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
+//     pop();
+//   }
